5bd4e3d864773dcd50d3d6f3f72b2daaf9225e0f
lwalmar <lwalmar@mail.ru>
2020-04-19 22:39:55 +0300

    add common cocmponent ButtonCircle

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment, useState, useEffect } from 'react';
 import './App.css';
+import Header from './components/Header';
+
+const loadData = () => new Promise((resolve, error) => setTimeout(() => resolve(['isin1', 'isin2', 'isin3']), 1000))
+
+const VIEW_AVAILABLE = [
+  'cost',
+  'yield',
+  'rating',
+  'payment'
+];
 
 function App() {
+  const [isins, setIsins] = useState([]);
+  const [view, setView] = useState('cost');
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const result = await loadData();
+      setIsins(result);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, ['']);
+
+  console.log('isins', isins)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="myPortfolio">
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        <Fragment>
+          <Header
+            viewActive={view}
+            viewAvailable={VIEW_AVAILABLE}
+            onChangeView={(view) => setView(view)}
+          />
+        </Fragment>
+      )}
     </div>
   );
 }
-
 export default App;
---
+++ src/components/Common/ButtonCircle/index.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import './style.css';
+
+const ButtonSquare = ({children}) => (
+  <div className="buttonCircle">
+    {children}
+  </div>
+);
+
+export default ButtonSquare;
