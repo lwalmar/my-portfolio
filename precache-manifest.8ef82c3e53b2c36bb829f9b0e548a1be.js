be3cee336bda2a81f25fd1bee3f9bbbd026be414
lwalmar <lwalmar@mail.ru>
2020-04-21 22:29:58 +0300

    Updates

---
+++ precache-manifest.8ef82c3e53b2c36bb829f9b0e548a1be.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "558e6dd97d4e5ebfd27a0739ada216b5",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "343f4dd4a2320d2f6d24",
+    "url": "/my-portfolio/static/css/main.01734bad.chunk.css"
+  },
+  {
+    "revision": "809ba5d675ce2871303b",
+    "url": "/my-portfolio/static/js/2.116eb21a.chunk.js"
+  },
+  {
+    "revision": "7ba7f2634383ff6ca36dc663e96d947c",
+    "url": "/my-portfolio/static/js/2.116eb21a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "343f4dd4a2320d2f6d24",
+    "url": "/my-portfolio/static/js/main.eb5e6937.chunk.js"
+  },
+  {
+    "revision": "c5d14653e6c493292441",
+    "url": "/my-portfolio/static/js/runtime-main.f13b90e6.js"
+  },
+  {
+    "revision": "562e58f2f0c0f0743b798d7e92ddbd2f",
+    "url": "/my-portfolio/static/media/arrow-down.562e58f2.svg"
+  },
+  {
+    "revision": "632f305d9a83c0a7d07b677fa1991b8b",
+    "url": "/my-portfolio/static/media/arrow-up.632f305d.svg"
+  },
+  {
+    "revision": "d88a56dc20886671cf62fb8ecb88ce1a",
+    "url": "/my-portfolio/static/media/calendar-picker--cross.d88a56dc.svg"
+  },
+  {
+    "revision": "4120449277eec0925e17eaab54454167",
+    "url": "/my-portfolio/static/media/discussion.41204492.svg"
+  },
+  {
+    "revision": "ba02094018e8b701046acd0c47acfd2b",
+    "url": "/my-portfolio/static/media/edit.ba020940.svg"
+  },
+  {
+    "revision": "ceb2a39168bd41e05c8b90e2ff85a20c",
+    "url": "/my-portfolio/static/media/gear.ceb2a391.svg"
+  },
+  {
+    "revision": "9921c1cda29186d77a8e3f349b547ad0",
+    "url": "/my-portfolio/static/media/info.9921c1cd.svg"
+  },
+  {
+    "revision": "48cc9e1779b94e7123f4ec58c037babf",
+    "url": "/my-portfolio/static/media/market.48cc9e17.svg"
+  },
+  {
+    "revision": "511396e75352611405467aa0b2b2a7e5",
+    "url": "/my-portfolio/static/media/portfolio.511396e7.svg"
+  }
+]);
